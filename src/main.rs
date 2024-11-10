0455fcaa974914d8c83954d86dab455cbfc5edf8
irfanshaik <irfanshaik11@gmail.com>
2024-11-11 01:11:30 +0700

    add mainnet & holesky

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,22 @@ use modules::{proposer_fetcher::ProposerFetcher,proposer_router::ProposerRouter}
 use config::AppConfig;
 
 #[derive(Template)]
-#[template(path = "proposer_stats.html")]
-struct ProposerStatsTemplate {
+#[template(path = "holesky.html")]
+struct ProposerStatsTemplateHolesky {
     bolt_proposers: Vec<Sidecar>,
     interstate_proposers: Vec<Sidecar>,
     current_slot: String,
 }
 
-async fn stats_handler(State(proposer_router): State<Arc<ProposerRouter>>) -> Html<String> {
+#[derive(Template)]
+#[template(path = "mainnet.html")]
+struct ProposerStatsTemplateMainnet {
+    bolt_proposers: Vec<Sidecar>,
+    interstate_proposers: Vec<Sidecar>,
+    current_slot: String,
+}
+
+async fn stats_handler_holesky(State(proposer_router): State<Arc<ProposerRouter>>) -> Html<String> {
     let sidecars = proposer_router.as_ref().get_sidecars().await;
     
     // Split and sort proposers by source
@@ -45,7 +53,36 @@ async fn stats_handler(State(proposer_router): State<Arc<ProposerRouter>>) -> Ht
     // Add current time
     let current_slot = proposer_router.get_latest_slot().await.unwrap_or(0).to_string();
 
-    let template = ProposerStatsTemplate {
+    let template = ProposerStatsTemplateHolesky {
+        bolt_proposers,
+        interstate_proposers,
+        current_slot,
+    };
+    
+    Html(template.render().unwrap_or_else(|_| String::from("Error rendering template")))
+}
+
+
+async fn stats_handler_mainnet(State(proposer_router): State<Arc<ProposerRouter>>) -> Html<String> {
+    let sidecars = proposer_router.as_ref().get_sidecars().await;
+    
+    // Split and sort proposers by source
+    let mut bolt_proposers: Vec<_> = sidecars.iter()
+        .filter(|s| s.source == "bolt")
+        .cloned()
+        .collect();
+    bolt_proposers.sort_by_key(|s| s.slot);
+
+    let mut interstate_proposers: Vec<_> = sidecars.iter()
+        .filter(|s| s.source == "interstate")
+        .cloned()
+        .collect();
+    interstate_proposers.sort_by_key(|s| s.slot);
+    
+    // Add current time
+    let current_slot = proposer_router.get_latest_slot().await.unwrap_or(0).to_string();
+
+    let template = ProposerStatsTemplateMainnet {
         bolt_proposers,
         interstate_proposers,
         current_slot,
@@ -81,7 +118,8 @@ async fn main() {
     .route("/api/v1/proposer", get(find_proposer_handler))
     .route("/api/v1/submit", post(submit_preconfirmation))
     // Add this new route
-    .route("/stats", get(stats_handler))
+    .route("/holesky", get(stats_handler_holesky))
+    .route("/mainnet", get(stats_handler_mainnet))
     .with_state(proposer_router);
     
     // Run ProposerFetcher in a separate task

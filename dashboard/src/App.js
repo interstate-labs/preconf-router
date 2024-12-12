cbf03a8f7a1a89dec19ef8738721c734b3ff7bf8
irfan <irfanshaik11@gmail.com>
2024-12-12 04:51:44 +0000

    feat: update UI for showing bolt proposers

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,47 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Proposers } from './components/Proposers.tsx';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let [proposers, setProposers] = useState([])
+    let [timestamp, setTimestamp] = useState("")
+
+    const updateProposers = async () => {
+        let res = await axios.get("http://135.181.191.125:58017/api/v1/proposers/lookahead?activeOnly=true&futureOnly=true");
+        // let json = await res.json();
+        setTimestamp(new Date().toLocaleString())
+        console.log(res.data)
+        setProposers(res.data);
+    }
+
+    useEffect(() => {
+        const id = setInterval(() => updateProposers(), [1000]);
+        return () => clearInterval(id)
+    }, [])
   return (
     <div className="App">
         <h1>Holesky Proposer Statistics</h1>
-        <div style="margin-bottom: 20px;">
-            <a href="/mainnet" style="margin-right: 20px;">Mainnet</a>
+        <div style={{marginBottom: 20}}>
+            <a href="/mainnet" style={{marginRight: 20}}>Mainnet</a>
             <a href="/holesky">Holesky</a>
         </div>
-        <h2>Available Aggregated Proposers || Last Updated: <span class="count">{{ timestamp }}</span></h2>
-        <h2>Total Proposers In Upcoming 32 Slots<span class="count">({{ bolt_proposers.len() + interstate_proposers.len()}})</span></h2>
+        <h2>Available Aggregated Proposers || Last Updated: <span class="count">{ timestamp }</span></h2>
+        <h2>Total Proposers In Upcoming 32 Slots<span class="count">{ proposers.length }</span></h2>
         
-        <div class="proposer-section">
-            <table>
-                <thead>
-                    <tr>
-                        <!-- <th>Type</th> enable to show which network proposers is under, not every network is on holesky rn--> 
-                        <th>Type</th>
-                        <th>Slot</th>
-                        <th>Validator Index</th>
-                        <th>URL</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {% for proposer in bolt_proposers %}
-                    <tr class="bolt">
-                        <td>Agg</td>
-                        <td>{{ proposer.slot }}</td>
-                        <td>{{ proposer.validator_index }}</td>
-                        <td>{{ proposer.sidecar_url }}</td>
-                    </tr>
-                    {% endfor %}
-                    {% for proposer in interstate_proposers %}
-                    <tr class="interstate">
-                        <td>Agg</td>
-                        <td>{{ proposer.slot }}</td>
-                        <td>{{ proposer.validator_index }}</td>
-                        <td>{{ proposer.sidecar_url }}</td>
-                    </tr>
-                    {% endfor %}
-                </tbody>
-            </table>
+        <div className="proposer-section">
+            <Proposers proposers={proposers} />
         </div>
     </div>
   );
